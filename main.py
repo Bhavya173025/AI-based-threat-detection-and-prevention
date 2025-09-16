adb905df3c21cb9432c0370ba75d548e666eecbe
Bhavya173025 <121882340+Bhavya173025@users.noreply.github.com>
2025-09-16 17:04:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ authenticator = stauth.Authenticate(
 # --------------------------
 # LOGIN PAGE TITLE
 # --------------------------
+st.image("assets/img.png", width=100)
 st.title("Sentinel-Auth")
 
 # --------------------------
